7bde2e7e9717f9b5600e154f7c3117a7b85013f8
bfelch <kagrenac@live.com>
2015-04-17 18:21:36 -0500

    Fixing Adam's :poop:
    Controller start

---
+++ Assets/Scripts/Movement/KeyboardMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardMove : MonoBehaviour {
+
+    private MoveController moveController;
+
+	// Use this for initialization
+	void Start () {
+        moveController = GetComponent<MoveController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float axisX = Input.GetAxis("horizontal");
+        float axisY = Input.GetAxis("vertical");
+
+        moveController.Move(axisX, axisY);
+	}
+}
---
+++ Assets/Scripts/Movement/MoveController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveController : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // -1 to 1 for each axis
+    // call this to move character
+    public void Move(float axisX, float axisY) {
+        Vector3 oldPos = transform.position;
+
+        float posX = oldPos.x;
+        float posY = oldPos.y;
+        float posZ = oldPos.z;
+
+        float newX = Mathf.Lerp(posX, posX + axisX * speed, Time.deltaTime);
+        float newY = Mathf.Lerp(posY, posY + axisY * speed, Time.deltaTime);
+
+        Vector3 newPos = new Vector3(newX, newY, oldPos.z);
+        transform.position = newPos;
+    }
+}
