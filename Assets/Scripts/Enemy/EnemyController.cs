92a51bd9aefdb25deaded59495c27764131668b2
KingWapo <Holcombea9172@my.uwstout.edu>
2015-04-17 20:36:29 -0500

    Enemy Spawning and flying forward

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    private float speed;
+
+	// Use this for initialization
+	void Start () {
+        speed = Random.Range(0.01f, 0.05f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * speed);
+	}
+
+    private void fire()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemySpawn.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawn : MonoBehaviour {
+
+    public GameObject EnemyPrefab;
+
+    private float spawnValue = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	    if (spawnSystem() <= 0)
+        {
+            spawnEnemy();
+            spawnValue = 100;
+        }
+	}
+
+    // Returns a value based off of an equation to
+    // determine if it is time to spawn an enemy or not
+    private float spawnSystem()
+    {
+        spawnValue--;
+
+        return spawnValue;
+    }
+
+    private void spawnEnemy()
+    {
+        GameObject enemy = (GameObject)Instantiate(EnemyPrefab);
+        Vector3 pos = transform.position;
+        float x = Random.Range(-20.0f, 20.0f);
+        float y = Random.Range(-20.0f, 20.0f);
+        float z = Random.Range(-20.0f, 20.0f);
+        pos += new Vector3(x, y, z);
+        enemy.transform.position = pos;
+        enemy.transform.rotation = transform.rotation;
+    }
+}
