0122b7ee2a68bfc79a8de6ab2a06e54115b891f5
bfelch <kagrenac@live.com>
2015-04-17 23:40:03 -0500

    STEAM MACHINE

--- Assets/Scripts/Movement/MoveController.cs
+++ Assets/Scripts/Movement/MoveController.cs
@@ -5,6 +5,9 @@ public class MoveController : MonoBehaviour {
 
     public float speed = 5;
 
+    private float boundX = 7.0f;
+    private float boundY = 4.0f;
+
     // -1 to 1 for each axis
     // call this to move character
     public void Move(float axisX, float axisY) {
@@ -13,8 +16,8 @@ public class MoveController : MonoBehaviour {
         float posX = oldPos.x;
         float posY = oldPos.y;
 
-        float newX = Mathf.Lerp(posX, posX + axisX * speed, Time.deltaTime);
-        float newY = Mathf.Lerp(posY, posY + axisY * speed, Time.deltaTime);
+        float newX = Mathf.Clamp(Mathf.Lerp(posX, posX + axisX * speed, Time.deltaTime), -boundX, boundX);
+        float newY = Mathf.Clamp(Mathf.Lerp(posY, posY + axisY * speed, Time.deltaTime), -boundY, boundY);
 
         Vector3 newPos = new Vector3(newX, newY, oldPos.z);
         transform.position = newPos;
