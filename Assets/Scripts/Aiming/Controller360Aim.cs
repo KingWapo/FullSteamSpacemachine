334a5d8e5ea70f7c4dc946ce216f3c70e1c19a7a
bfelch <kagrenac@live.com>
2015-04-17 22:34:14 -0500

    Renaming heirs

--- Assets/Scripts/Aiming/Controller360Aim.cs
+++ Assets/Scripts/Aiming/Controller360Aim.cs
@@ -3,16 +3,11 @@ using System.Collections;
 
 public class Controller360Aim : AimController {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update() {
         float axisX = Input.GetAxis("360 Right Horizontal");
         float axisY = Input.GetAxis("360 Right Vertical");
 
         Aim(axisX, axisY);
-	}
+    }
 }
